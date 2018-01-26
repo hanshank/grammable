4a475e74cd2b09d64497961b1228546dbc4bb72d
Your Name <hans@hanken.org>
2018-01-26 23:32:05 +0000

    Before lesson 13 test writing

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+
+  describe "grams#edit action" do
+  it "should successfully show the edit form if the gram is found" do
+
+  end
+
+  it "should return a 404 error message if the gram is not found" do
+
+  end
+end
+
   describe "grams#show action" do
   it "should successfully show the page if the gram is found" do
     gram = FactoryBot.create(:gram)
