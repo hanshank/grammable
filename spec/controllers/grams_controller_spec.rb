b4c967657648ae025060d2c359abbe9f2fbfd461
Your Name <hans@hanken.org>
2018-01-27 18:07:23 +0000

    Validation of image working

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -160,7 +160,13 @@ end
       user = FactoryBot.create(:user)
       sign_in user
 
-      post :create, params: { gram: { message: 'Hello!' } }
+      post :create, params: {
+        gram: {
+          message: 'Hello!',
+          picture: fixture_file_upload("/picture.png", 'image/png')
+        }
+      }
+      
       expect(response).to redirect_to root_path
 
       gram = Gram.last
