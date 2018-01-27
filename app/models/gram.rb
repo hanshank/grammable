de14fa44cb2eeb6341bab2b77fb1c74cffa800de
Your Name <hans@hanken.org>
2018-01-27 13:36:55 +0000

    Heroku error

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,6 +1,8 @@
 class Gram < ApplicationRecord
-  mount_uploader :picture, PictureUploader
   validates :message, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :picture, presence: true
+
+  mount_uploader :picture, PictureUploader
 
   belongs_to :user
 end
