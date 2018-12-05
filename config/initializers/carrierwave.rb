68a63b5378c9ba52fa5487d32629c0c612c3f7da
hanshank <hans@hanken.org>
2018-12-05 01:24:48 -0800

    new aws connection points

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+    region:                'us-west-1'                   # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory  = ENV["AWS_BUCKET"]
 end
